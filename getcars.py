f4708a7d71edd10bfb9a3fd671ea8833212e535c
Maurizio Napolitano <napo@fbk.eu>
2025-02-02 20:14:25 +0100

    Update getcars.py

--- getcars.py
+++ getcars.py
@@ -17,10 +17,8 @@ import cv2
 import numpy as np
 
 import areas.areas as ar
-# workaround https://github.com/pytorch/vision/issues/4156
-# torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
-#model = torch.hub.load('ultralytics/yolov8', 'yolov8n')  # or yolov5m, yolov5l, yolov5x, custom
-torch.serialization.add_safe_globals([DetectionModel])
+import torch.nn.modules.container
+torch.serialization.add_safe_globals([DetectionModel, torch.nn.modules.container.Sequential])
 model = YOLO("yolov8n.pt")
 warnings.filterwarnings('ignore')
 #locale.setlocale(locale.LC_TIME, 'it_IT.utf8')
