7cae9fadfdb0b8dca27c53ccb59e6d19fc38fc39
Maurizio Napolitano <napo@fbk.eu>
2025-02-09 08:49:23 +0100

    Update getcars.py

--- getcars.py
+++ getcars.py
@@ -19,6 +19,7 @@ import numpy as np
 import areas.areas as ar
 import torch.nn.modules.container
 torch.serialization.add_safe_globals([DetectionModel, torch.nn.modules.container.Sequential])
+torch.serialization.default_load_weights_only = False
 model = YOLO("yolov8n.pt")
 warnings.filterwarnings('ignore')
 #locale.setlocale(locale.LC_TIME, 'it_IT.utf8')
